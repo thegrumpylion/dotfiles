961176fff821e35499aa27c94199ba245e7b8955
nikolas sepos <nikolas.sepos@gmail.com>
2022-04-09 18:07:29 +0300

    update lvim zshrc

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -1,7 +1,6 @@
 local dap = require "dap"
 local set = vim.opt
-
-set.relativenumber = true
+local o = vim.o
 
 -- general
 lvim.log.level = "warn"
@@ -97,18 +96,7 @@ lvim.builtin.treesitter.ensure_installed = {
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
--- generic LSP settings
-
--- ---@usage disable automatic installation of servers
--- lvim.lsp.automatic_servers_installation = false
-
--- ---@usage Select which servers should be configured manually. Requires `:LvimCacheReset` to take effect.
--- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
--- vim.list_extend(lvim.lsp.override, { "pyright" })
-
--- ---@usage setup a server -- see: https://www.lunarvim.org/languages/#overriding-the-default-configuration
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pylsp", opts)
+lvim.builtin.lualine.style = "default"
 
 -- -- you can set a custom on_attach function that will be used for all the language servers
 -- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
@@ -229,4 +217,7 @@ dap.configurations.cs = {
     end,
   },
 }
+
+set.relativenumber = true
+o.laststatus = 3
 
