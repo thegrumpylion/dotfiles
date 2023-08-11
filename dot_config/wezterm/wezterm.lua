e41bbb1a6b176059267ce0db7da4978883b1e06e
Nikolas Sepos <nikolas.sepos@gmail.com>
2023-08-11 15:01:20 +0300

    wezterm ctrl-shift-e, starship argocd

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -10,14 +10,40 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
 -- For example, changing the color scheme:
 config.color_scheme = "nord"
 
 config.font = wezterm.font("FiraCode Nerd Font Mono")
 
 config.enable_tab_bar = false
+
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- make ARCH0 jira issue ids clickable
+table.insert(config.hyperlink_rules, {
+	regex = [[\bARCH0-(\d+)\b]],
+	format = "https://platformcy.atlassian.net/browse/ARCH0-$1",
+})
+
+config.keys = {
+	{
+		key = "E",
+		mods = "CTRL",
+		action = wezterm.action({
+			QuickSelectArgs = {
+				patterns = {
+					"https?://\\S+",
+				},
+				action = wezterm.action_callback(function(window, pane)
+					local url = window:get_selection_text_for_pane(pane)
+					wezterm.log_info("opening: " .. url)
+					wezterm.open_with(url)
+				end),
+			},
+		}),
+	},
+}
 
 -- and finally, return the configuration to wezterm
 return config
