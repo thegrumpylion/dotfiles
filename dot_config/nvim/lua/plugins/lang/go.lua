01c77c4d7f97c05ad09cc4e70234b540d59e7dd0
Nikolas Sepos <nikolas.sepos@gmail.com>
2023-10-22 20:39:12 +0300

    update go

--- dot_config/nvim/lua/plugins/lang/go.lua
+++ dot_config/nvim/lua/plugins/lang/go.lua
@@ -13,14 +13,17 @@ return {
   -- Add go.nvim
   {
     "ray-x/go.nvim",
-    dependencies = {
+    dependencies = { -- optional packages
       "ray-x/guihua.lua",
       "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
     },
-    config = true,
-    event = "CmdlineEnter",
-    ft = { "go", "gomod", "gosum", "gowork" },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
 
   -- Add gopher.nvim
